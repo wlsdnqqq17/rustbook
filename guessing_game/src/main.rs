a81a42896db174d0a9173cb2b4e99b7d87988062
wlsdnqqq17 <wlsdnqqq17@gmail.com>
2024-09-15 12:48:31 +0000

    Allowing multuple guesses

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,4 +1,5 @@
 use std::io;
+use std::cmp::Ordering;
 use rand::Rng;
 
 fn main() {
@@ -7,14 +8,26 @@ fn main() {
     let secret_number = rand::thread_rng().gen_range(1..=100);
 
     println!("The secret number is: {secret_number}");
-    
-    println!("Please input your guess.");
 
-    let mut guess = String::new();
+    loop{    
+        println!("Please input your guess.");
+        let mut guess = String::new();
+        
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
+        let guess: u32 = guess.trim().parse().expect("Please type a number!");
+        
+        println!("You guessed: {guess}");
 
-    println!("You guessed: {guess}");
+        match guess.cmp(&secret_number) {
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Less => println!("Too small!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+    }    
 }
